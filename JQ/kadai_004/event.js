374734eee9340aea483263b71027b4761d07dcf8
oshiromisuki <6igcastleman@gmail.com>
2024-11-30 09:19:36 +0900

    commit

---
+++ JQ/kadai_004/event.js
@@ -0,0 +1,11 @@
+// load時の処理
+$(window).on('load', function() {
+  console.log('loadイベントが発生しました');
+});
+
+// scroll時の処理
+$(window).on('scroll', function() {
+  console.log('scrollイベントが発生しました');
+});
+
+
